075408162d61590384dbd8853a85a6b3cefa8b0c
DarlynYami05 <dwverling@gmail.com>
2021-03-05 10:38:57 -0500

    Movimiento de camara

--- app/src/main/java/software/reconocerpaises/MainActivity.java
+++ app/src/main/java/software/reconocerpaises/MainActivity.java
@@ -2,22 +2,65 @@ package software.reconocerpaises;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.style.TtsSpan;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mapa;
+    PolylineOptions poligonos;
+    LatLng[] puntos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        * Spinner spinner = (Spinner) findViewById(R.id.spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
-ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-        R.array.planets_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-spinner.setAdapter(adapter);*/
+
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.mapa);
+        mapFragment.getMapAsync(this);
+
+        Spinner combobox = (Spinner)findViewById(R.id.combobox);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.paises, R.layout.activity_main);
+        combobox.setAdapter(adapter);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.poligonos = new PolylineOptions();
+        this.mapa = googleMap;
+
+        this.puntos = new LatLng[]{new LatLng(-1.831239, -78.183406)}; //Ecuador
+        this.mapa.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        this.mapa.getUiSettings().setZoomControlsEnabled(true);
+
+        CameraUpdate camara = CameraUpdateFactory.newLatLngZoom(puntos[0], 6);
+        this.mapa.moveCamera(camara);
+    }
+
+    public void cambiarLugar(int lugar){
+        int zoom = 14;
+
+        CameraPosition posicionCamara = new CameraPosition.Builder()
+                .target(this.puntos[lugar])
+                .zoom(zoom)
+                .bearing(45)
+                .build();
+
+        CameraUpdate camara = CameraUpdateFactory.newCameraPosition(posicionCamara);
+        this.mapa.animateCamera(camara);
     }
 }
